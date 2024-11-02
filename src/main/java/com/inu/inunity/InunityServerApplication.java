4ad273c46772f67241a8d7004dc551bfb365dde0
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-11-03 03:20:52 +0900

    chore: 초기 springboot 파일 생성

---
+++ src/main/java/com/inu/inunity/InunityServerApplication.java
@@ -0,0 +1,13 @@
+package com.inu.inunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InunityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InunityServerApplication.class, args);
+	}
+
+}
