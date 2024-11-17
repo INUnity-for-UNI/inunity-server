20ffcd59f83bf19d5b459dcfb11c2dcc30943687
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-11-18 06:45:30 +0900

    feat: JWT token 관리 및 생성/검증 기능 추가#6

---
+++ src/main/java/com/inu/inunity/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.inu.inunity.security;
+
+import com.inu.inunity.domain.User.User;
+import com.inu.inunity.domain.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String studentNumber) {
+        User user = userRepository.findByStudentId(studentNumber)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다."));
+        return CustomUserDetails.from(user);
+    }
+
+
+}
---
+++ src/main/java/com/inu/inunity/security/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.inu.inunity.security.config;
+
+public class SecurityConfig {
+}
