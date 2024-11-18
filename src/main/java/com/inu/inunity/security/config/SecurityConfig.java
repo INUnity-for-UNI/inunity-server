9654b08a0d3b600d869ff779c9449266a525b49e
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-11-18 22:36:52 +0900

    feat: sequrity 설정 및 권한 예외처리#6

--- src/main/java/com/inu/inunity/security/config/SecurityConfig.java
+++ src/main/java/com/inu/inunity/security/config/SecurityConfig.java
@@ -1,4 +1,59 @@
 package com.inu.inunity.security.config;
 
+import com.inu.inunity.security.JwtAuthFilter;
+import com.inu.inunity.security.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    private final AccessDeniedHandler accessDeniedHandler;
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .cors(security -> {
+                    security.configurationSource(corsConfigurationSource);
+                })
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("**/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthFilter(jwtProvider),
+                        UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
+                        .accessDeniedHandler(accessDeniedHandler)
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                );
+
+
+        return httpSecurity.build();
+    }
 }
