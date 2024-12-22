35fa0bb82cbfaf6c4a2b5fa88089143cb0c526fb
squidjiny <92552047+squidjiny@users.noreply.github.com>
2024-12-22 17:24:32 +0900

    feat: 연락 수단과 기술을 enum으로 관리 및 데이터베이스에 칼럼으로 추가#9

---
+++ src/main/java/com/inu/inunity/domain/profile/contract/ContractType.java
@@ -0,0 +1,8 @@
+package com.inu.inunity.domain.profile.contract;
+
+public enum ContractType {
+    INSTAGRAM,
+    GITHUB,
+    KAKAOTALK,
+    BLOG
+}
---
+++ src/main/java/com/inu/inunity/domain/profile/skill/SkillType.java
@@ -0,0 +1,86 @@
+package com.inu.inunity.domain.profile.skill;
+
+public enum SkillType {
+    // 프로그래밍 언어
+    JAVA,
+    PYTHON,
+    JAVASCRIPT,
+    TYPESCRIPT,
+    C,
+    CPP, // C++
+    CSHARP, // C#
+    KOTLIN,
+    SWIFT,
+    GO,
+    RUBY,
+    PHP,
+
+    // 백엔드 프레임워크
+    SPRING_BOOT,
+    SPRING,
+    DJANGO,
+    FLASK,
+    EXPRESS,
+    NESTJS,
+    RUBY_ON_RAILS,
+
+    // 프론트엔드(웹) 프레임워크
+    REACT,
+    ANGULAR,
+    VUE,
+    NEXTJS,
+    SVELTE,
+
+    // 프론트엔드(모바일) 프레임워크
+    ANDROID,
+    FLUTTER,
+    REACT_NATIVE,
+    SWIFT_UI,
+
+    // 데브옵스
+    DOCKER,
+    KUBERNETES,
+    AWS,
+    AZURE,
+    GOOGLE_CLOUD,
+    JENKINS,
+    GITHUB_ACTIONS,
+
+    // 데이터베이스
+    MYSQL,
+    POSTGRESQL,
+    MONGODB,
+    REDIS,
+    ELASTICSEARCH,
+    ORACLE,
+    SQL,
+
+    // 테스트 코드
+    JUNIT,
+    SELENIUM,
+    CYPRESS,
+    PUPPETEER,
+
+    // 협업 도구
+    GIT,
+    GITHUB,
+    GITLAB,
+    JIRA,
+    CONFLUENCE,
+    SLACK,
+
+    // 데사
+    TENSORFLOW,
+    PYTORCH,
+    SCIKIT_LEARN,
+    PANDAS,
+    NUMPY,
+    SPARK,
+
+    // 기타
+    HTML,
+    CSS,
+    SASS,
+    TAILWIND,
+    BOOTSTRAP
+}
