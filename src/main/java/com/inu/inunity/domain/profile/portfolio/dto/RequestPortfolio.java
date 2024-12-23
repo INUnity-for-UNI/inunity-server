8bb196505a84a3fd942044ac0bef6d530c136538
squidjiny <92552047+squidjiny@users.noreply.github.com>
2024-12-24 04:23:23 +0900

    feat: portfolio request/respone dto 선언#9

---
+++ src/main/java/com/inu/inunity/domain/profile/portfolio/dto/RequestPortfolio.java
@@ -0,0 +1,4 @@
+package com.inu.inunity.domain.profile.portfolio.dto;
+
+public record RequestPortfolio(Long portfolioId, String url) {
+}
---
+++ src/main/java/com/inu/inunity/domain/profile/portfolio/dto/ResponsePortfolio.java
@@ -0,0 +1,4 @@
+package com.inu.inunity.domain.profile.portfolio.dto;
+
+public record ResponsePortfolio(Long portfolioId, String url) {
+}
