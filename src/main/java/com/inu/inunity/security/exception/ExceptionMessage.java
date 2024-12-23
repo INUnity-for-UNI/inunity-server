f48de1392d0f2641ac132fd1b8ee5215d2b3acbb
squidjiny <92552047+squidjiny@users.noreply.github.com>
2024-12-24 02:27:10 +0900

    feat: SkillService(CRUD) 구현 완료#9

--- src/main/java/com/inu/inunity/security/exception/ExceptionMessage.java
+++ src/main/java/com/inu/inunity/security/exception/ExceptionMessage.java
@@ -21,7 +21,8 @@ public enum ExceptionMessage {
     PORTAL_LOGIN_FAILED_ID_PASSWORD_INCORRECT("포탈 로그인 실패: 아이디비번 틀림", 0, HttpStatus.BAD_REQUEST),
     PORTAL_LOGIN_FAILED_SERVER_ERROR("포탈 로그인 실패: 예기치않은 서버 통신 오류", 0, HttpStatus.BAD_REQUEST),
     CONTRACT_NOT_FOUND("SNS/Link 존재하지 않습니다.", 0, HttpStatus.NOT_FOUND),
-    CAREER_NOT_FOUND("경력 이력이 존재하지 않습니다.", 0, HttpStatus.NOT_FOUND)
+    CAREER_NOT_FOUND("경력 이력이 존재하지 않습니다.", 0, HttpStatus.NOT_FOUND),
+    SKILL_NOT_FOUND("스킬이 존재하지 않습니다.", 0, HttpStatus.NOT_FOUND)
     ;
 
 
