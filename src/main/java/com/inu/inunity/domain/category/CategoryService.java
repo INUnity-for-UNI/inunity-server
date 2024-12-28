ec4bbd7e980bd5afcf2db38d87df60e0d4fdf7bd
NARUBROWN <codingshark@inu.ac.kr>
2024-12-28 18:44:40 +0900

    [Feat] 카테고리 Controller 계층 작성 완료

--- src/main/java/com/inu/inunity/domain/category/CategoryService.java
+++ src/main/java/com/inu/inunity/domain/category/CategoryService.java
@@ -1,8 +1,13 @@
 package com.inu.inunity.domain.category;
 
+import com.inu.inunity.domain.article.Article;
+import com.inu.inunity.domain.article.ArticleRepository;
 import com.inu.inunity.domain.category.dto.RequestCreateCategory;
+import com.inu.inunity.domain.category.dto.ResponseArticleForList;
 import com.inu.inunity.domain.category.dto.ResponseCategory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final ArticleRepository articleRepository;
 
     /**
      * 카테고리 생성 메서드
@@ -18,7 +24,7 @@ public class CategoryService {
      * @param requestCreateCategory Record Class
      * @return Long 생성된 Category ID
      */
-    public Long createCategory(RequestCreateCategory requestCreateCategory) {
+    Long createCategory(RequestCreateCategory requestCreateCategory) {
         Category newCategory = Category.builder()
                 .name(requestCreateCategory.name())
                 .description(requestCreateCategory.description())
@@ -33,7 +39,7 @@ public class CategoryService {
      * @author 김원정
      * @return List<ResponseCategory> List 클래스에 감싸진 ResponseCategory
      */
-    public List<ResponseCategory> findAllCategories() {
+    List<ResponseCategory> findAllCategories() {
         List<Category> foundCategories = categoryRepository.findAll();
         return foundCategories.stream().map(category -> ResponseCategory.builder()
                  .id(category.getId())
@@ -50,7 +56,7 @@ public class CategoryService {
      * @param category_name 바뀔 Category 이름
      * @return Long 수정된 Category ID
      */
-    public Long editCategoryName(Long category_id, String category_name) {
+    Long editCategoryName(Long category_id, String category_name) {
         Category foundCategory = categoryRepository.findById(category_id)
                 .orElseThrow(() -> new RuntimeException("Category not found"));
         foundCategory.editName(category_name);
@@ -65,7 +71,7 @@ public class CategoryService {
      * @param status 바뀔 Category의 활성 상태
      * @return Long 수정된 Category ID
      */
-    public Long changeStatus(Long category_id, boolean status) {
+    Long changeStatus(Long category_id, boolean status) {
         Category foundCategory = categoryRepository.findById(category_id)
                 .orElseThrow(() -> new RuntimeException("Category not found"));
         foundCategory.changeStatus(status);
@@ -78,7 +84,7 @@ public class CategoryService {
      * @author 김원정
      * @param category_id Category ID
      */
-    public void deleteCategory(Long category_id) {
+    void deleteCategory(Long category_id) {
         categoryRepository.deleteById(category_id);
     }
 
@@ -89,4 +95,14 @@ public class CategoryService {
      * @param category_id 카테고리 ID
      * @return page 클래스에 감싸진 responseArticleForList 클래스
      */
+    Page<ResponseArticleForList> getArticles(Long category_id, Pageable pageable) {
+        Page<Article> pagingArticle = articleRepository.findAllByCategoryId(category_id, pageable);
+        return pagingArticle.map(article -> ResponseArticleForList.builder()
+                .id(article.getId())
+                .title(article.getTitle())
+                .isAnonymous(article.getIsAnonymous())
+                .view(article.getView())
+                .isDeleted(article.getIsDeleted())
+                .build());
+    }
 }
