120ff96e375943e4bef12c308b94508200426f35
squidjiny <92552047+squidjiny@users.noreply.github.com>
2025-01-05 07:36:18 +0900

    refactor: profileController 줄바꿈#9

--- src/main/java/com/inu/inunity/domain/profile/ProfileController.java
+++ src/main/java/com/inu/inunity/domain/profile/ProfileController.java
@@ -15,26 +15,31 @@ import org.springframework.web.bind.annotation.*;
 public class ProfileController {
     private final ProfileService profileService;
     @PostMapping("/users/{userid}/profile/career")
-    public ResponseEntity<CommonResponse<?>> createUserCareer(@RequestBody RequestCreateCareer request,
-                                                          @PathVariable Long userid, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<CommonResponse<?>> createUserCareer(@RequestBody RequestCreateCareer request, @PathVariable Long userid,
+                                                              @AuthenticationPrincipal UserDetails userDetails) {
         profileService.createCareer(request, userid, userDetails);
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success("career 생성 성공", null));
     }
 
     @GetMapping("/users/{userid}/profile/career")
-    public CommonResponse<?> getUserCareers(@PathVariable Long userid, @AuthenticationPrincipal UserDetails userDetails) {
-        return CommonResponse.success("career 생성 성공", profileService.getCareers(userDetails));
+    public CommonResponse<?> getUserCareers(@PathVariable Long userid,
+                                            @AuthenticationPrincipal UserDetails userDetails) {
+        return CommonResponse.success("유저의 career 가져오기 성공", profileService.getCareers(userDetails));
     }
 
     @PutMapping("/users/{userid}/profile/career")
-    public CommonResponse<?> updateUserCareers(@RequestBody RequestUpdateCareer request, @PathVariable Long userid, @AuthenticationPrincipal UserDetails userDetails) {
+    public CommonResponse<?> updateUserCareers(@RequestBody RequestUpdateCareer request, @PathVariable Long userid,
+                                               @AuthenticationPrincipal UserDetails userDetails) {
         profileService.updateCareer(request, userid, userDetails);
         return CommonResponse.success("career 수정 성공", null);
     }
 
     @DeleteMapping("/users/{userid}/profile/career/{careerid}")
-    public CommonResponse<?> deleteUserCareers(@PathVariable Long careerid, @PathVariable Long userid, @AuthenticationPrincipal UserDetails userDetails) {
+    public CommonResponse<?> deleteUserCareers(@PathVariable Long careerid, @PathVariable Long userid,
+                                               @AuthenticationPrincipal UserDetails userDetails) {
         profileService.deleteCareer(careerid, userid, userDetails);
         return CommonResponse.success("career 삭제 성공", null);
     }
+
+
 }
