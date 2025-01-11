1bf9c8ad89416f3e5c26e7e902d40c68371cd216
squidjiny <92552047+squidjiny@users.noreply.github.com>
2025-01-11 09:37:06 +0900

    feat: 삭제된 article이 article 목록 조회에서 나오는 버그 수정#17

--- src/main/java/com/inu/inunity/domain/article/ArticleRepository.java
+++ src/main/java/com/inu/inunity/domain/article/ArticleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Page<Article> findAllByCategoryId(Long categoryId, Pageable pageable);
+    Page<Article> findAllByCategoryIdAndIsDeletedIsFalse(Long categoryId, Pageable pageable);
     Page<Article> findAllByUserIdAndIsDeletedIsFalse(Long userId, Pageable pageable);
 }
