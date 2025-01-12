aa24230a45951a260c64caf960ffcd8e39aa3225
Wonjeong Kim <narubrown@users.noreply.github.com>
2025-01-12 18:52:11 +0900

    [Feat] 진행상황 저장

--- src/main/java/com/inu/inunity/util/communicate/CommunicateUtil.java
+++ src/main/java/com/inu/inunity/util/communicate/CommunicateUtil.java
@@ -3,8 +3,14 @@ package com.inu.inunity.util.communicate;
 import com.inu.inunity.util.communicate.dto.ResponseCleanBot;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -12,13 +18,15 @@ import org.springframework.web.client.RestClient;
 public class CommunicateUtil {
     private final RestClient restClientForAI;
 
-    public String requestToCleanBot(String text) {
-        ResponseCleanBot result = restClientForAI.get()
-                .uri(uriBuilder -> uriBuilder.path("/cleanBot")
-                        .queryParam("text", text)
-                        .build())
+    public Boolean requestToCleanBot(String text) {
+        Map<String, String> request = new HashMap<>();
+        request.put("text", text);
+        ResponseCleanBot result = restClientForAI.post()
+                .uri("/cleanBot")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
                 .retrieve()
                 .body(ResponseCleanBot.class);
-        return result.getFiltered_text();
+        return Objects.requireNonNull(result).isBinary_class();
     }
 }
