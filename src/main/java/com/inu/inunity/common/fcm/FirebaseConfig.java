462e5fea99cb9bf77f89bc4ad34e6dcd4fd8d4f2
squidjiny <92552047+squidjiny@users.noreply.github.com>
2025-01-14 18:15:13 +0900

    fix: 운영 환경에서 fcm 경로를 읽지 못하는 현상 수정#24

--- src/main/java/com/inu/inunity/common/fcm/FirebaseConfig.java
+++ src/main/java/com/inu/inunity/common/fcm/FirebaseConfig.java
@@ -4,9 +4,9 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
@@ -14,11 +14,12 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void initializeFirebase() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/fcm-service.json");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ClassPathResource("fcm-service.json").getInputStream()
+        );
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(credentials)
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
